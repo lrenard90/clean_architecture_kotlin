52c572652386c733d67e91db03ec7c4f54c61547
lrenard90 <lucas.renard90@gmail.com>
2023-01-24 23:11:46 +0100

    chore: update

--- clean-architecture-domain/src/main/kotlin/fr/renard/clean_architecture_domain/product/usecases/CreateProductUseCase.kt
+++ clean-architecture-domain/src/main/kotlin/fr/renard/clean_architecture_domain/product/usecases/CreateProductUseCase.kt
@@ -4,7 +4,9 @@ import fr.renard.clean_architecture_domain.product.ports.ProductRepository
 import fr.renard.clean_architecture_domain.product.usecases.boundary.dto.ProductCreationRequest
 import fr.renard.clean_architecture_domain.product.usecases.boundary.dto.ProductCreationResponse
 import fr.renard.clean_architecture_domain.product.usecases.boundary.mapper.CreateProductUseCaseConverter
+import fr.renard.clean_architecture_domain.socle.dependency_injection.annotation.UseCase
 
+@UseCase
 class CreateProductUseCase(
     private var productRepository: ProductRepository,
     private val createProductUseCaseConverter: CreateProductUseCaseConverter
